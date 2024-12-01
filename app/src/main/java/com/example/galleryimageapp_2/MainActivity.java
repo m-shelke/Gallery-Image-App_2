b4e83b3f8afb85018aac2785eaa95e63f1e38e10
AtomicFive4059 <mshelke074@gmail.com>
2024-12-01 21:12:01 +0530

    Camera Image and Gallery Image picked

--- app/src/main/java/com/example/galleryimageapp_2/MainActivity.java
+++ app/src/main/java/com/example/galleryimageapp_2/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.galleryimageapp_2;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    CardView cameraCardView,galleryCardView;
+    ImageView imageView;
+
+    private final int CAM_REQ = 100;
+    private final int IMG_REQ = 100;
+
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +37,51 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        cameraCardView = findViewById(R.id.cameraCardView);
+        galleryCardView = findViewById(R.id.galleryCardView);
+        imageView = findViewById(R.id.imageView);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        cameraCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,CAM_REQ);
+            }
+        });
+
+        galleryCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent,IMG_REQ);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK){
+
+            if (requestCode == CAM_REQ){
+
+                Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                imageView.setImageBitmap(bitmap);
+
+            }if (requestCode == IMG_REQ) {
+
+                imageUri = data.getData();
+                imageView.setImageURI(imageUri);
+            }
+        }
     }
 }
